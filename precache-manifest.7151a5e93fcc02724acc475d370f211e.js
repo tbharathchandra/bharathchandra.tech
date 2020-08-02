4d460e1426f294c1f6496dacc133133d5b808fa5
tbharathchandra <t.bharathchandra@gmail.com>
2020-08-02 13:23:37 +0530

    new ui

---
+++ precache-manifest.7151a5e93fcc02724acc475d370f211e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ecabc70204a7abf9d883a6222a3c014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7f651a0435de465ce29",
+    "url": "/static/css/main.a943833b.chunk.css"
+  },
+  {
+    "revision": "9a2cda652a83d49d00ae",
+    "url": "/static/js/2.46566f2e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.46566f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7f651a0435de465ce29",
+    "url": "/static/js/main.8d2cdcda.chunk.js"
+  },
+  {
+    "revision": "c08a779116a3ed3c840c",
+    "url": "/static/js/runtime-main.29fc6ffc.js"
+  },
+  {
+    "revision": "d1ccea319b88f4dedf586a373d3c4e6e",
+    "url": "/static/media/thumb.d1ccea31.png"
+  }
+]);
